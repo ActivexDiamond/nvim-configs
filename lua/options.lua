68088f97563d6b73b8785f79f432bb4930e224a5
ActivexDiamond <activexdiamond@gmail.com>
2025-11-16 16:12:08 +0300

    fix: minor typo

--- lua/options.lua
+++ lua/options.lua
@@ -147,14 +147,17 @@ vim.api.nvim_create_autocmd("CursorMoved", {
 	group = "HighlightActiveWindow",
 })
 
+--TODO: Actually finish setting up gf to work with terminal errors and what not.
+
+--[[
 vim.api.nvim_create_autocmd("BufEnter", {
 	desc = "Update `path` whenever `cwd` is updated so things like gf work nicely.",
 	group = vim.api.nvim_create_augroup("SyncPathToCwd", { clear = true }),
 	callback = function()
 		local cwd = vim.fn.getcwd()
 		cwd = vim.fn.isdirectory(cwd) and cwd or vim.fn.fnamemodify(cwd, ':h')
-		vim.cmd("set path=" .. cwd .. "/**")	
+		vim.cmd("set path='" .. cwd .. "/**'")	
 	end
 })
-
+--]]
 
